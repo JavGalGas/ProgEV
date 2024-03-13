28aa5c95cd94b72cf2900b8b62efa014521a9373
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-03-13 18:53:08 +0100

    Add files via upload

---
+++ PROG/EV2/no_evaluable/Basura4/Basura4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Basura4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result1=0.0;
+            double result2=0.0;
+            Tuple.Ecuation(2.1, 5.0, 2.0, out result1, out result2);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+    }
+}
---
+++ PROG/EV2/no_evaluable/Basura4/Basura4/Tuple.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basura4
+{
+    public class Tuple
+    {
+
+        //(int, string) _tupleEx1 = (4,"hola");
+        //Tuple<int, string> _tupleEx2 = new(5,"adios");
+
+        //(r1, r2)
+        //out r1, out r2
+        //             __________
+        //      -b +- v b^2 -4ac
+        //x=    -----------------
+        //              2a
+        public static void Ecuation(double a, double b, double c, out double result1, out double result2)
+        {
+            SolveEquation2(a, b, c, out result1, out result2);
+            //result1 = (-b + Math.Sqrt(Math.Pow(b,2) - 4.0 * a * c)) / (2.0 * a);
+            //result2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4.0 * a * c)) / (2.0 * a);
+            //var result = ((-b + Math.Sqrt(Math.Pow(b, 2) - 4*a * c)/(2*a)), (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c) / (2 * a)));
+
+        }
+        public static bool SolveEquation2(double a, double b, double c, out double result1, out double result2)
+        {
+            result1 = double.NaN;
+            result2 = double.NaN;
+            if (a == 0.0)
+                return false;
+            double r = Math.Pow(b, 2) - 4 * a * c;
+            if ((Math.Pow(b, 2) - 4 * a * c) < 0)
+                return false;
+            double root = Math.Sqrt(r);
+            double denom = 1.0 / (2.0 * a);
+            result1 = (-b + root) * denom;
+            result2 = (-b - root) * denom;
+            return true;
+        }
+
+
+    }
+}
