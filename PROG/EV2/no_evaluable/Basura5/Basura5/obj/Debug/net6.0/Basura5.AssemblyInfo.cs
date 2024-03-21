d8ca1a2cae01eff5190aad403a0b7ef55c13e3d9
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-03-21 20:05:54 +0100

    copia 13

--- PROG/EV2/no_evaluable/Basura5/Basura5/obj/Debug/net6.0/Basura5.AssemblyInfo.cs
+++ PROG/EV2/no_evaluable/Basura5/Basura5/obj/Debug/net6.0/Basura5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basura5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7523fe26af149c0b16bce559f1c7fbe6d72766a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basura5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basura5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
