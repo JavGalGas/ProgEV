4774e41df81aa760cb98959afefd54f59c884e8f
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-04-02 18:51:08 +0200

    copia 14

---
+++ PROG/EV3/Pathfinding/Pathfinding/ChangeNameImage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pathfinding
+{
+    public class ChangeNameImage
+    {
+        public static void CopyImageFile(string filePath) // perfilar con la página web guardada en Marcadores
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string extension = Path.GetExtension(filePath).ToLower();
+
+                    // Verifica si el archivo es una imagen
+                    if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif" || extension == ".bmp")
+                    {
+                        string directory = Path.GetDirectoryName(filePath);
+                        string fileName = Path.GetFileNameWithoutExtension(filePath);
+                        DateTime creationTime = File.GetCreationTime(filePath);
+                        string newFileName = $"{creationTime:yyyy-MM-dd-HH-mm-ss}_{fileName}{extension}";
+                        string newFilePath = Path.Combine(directory, newFileName);
+
+                        // Realiza una copia de la imagen
+                        File.Copy(filePath, newFilePath);
+
+                        Console.WriteLine($"Se ha creado una copia de la imagen en: {newFilePath}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"El archivo '{filePath}' no es una imagen.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"El archivo '{filePath}' no existe.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
---
+++ PROG/EV3/Pathfinding/Pathfinding/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+namespace Pathfinding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//pasar a hacer nuevos arrays de string en args
+
+            //consigue la ruta absoluta del programa en ejecución (ddl)
+            string programAbsolutePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            //ruta relativa del directorio Pathfinding a partir de programAbsolutePath
+            string relativePath = @"..\..\..\..\..\";
+
+            List<string> entryRoutes = new List<string>
+            {
+                @"entry1\",
+                @"entry2\"
+            };
+            string exitRoute = @"exit\";            
+
+            args = new string[entryRoutes.Count*2 + 2];
+
+            for (int i = 0; i < args.Length - 2; i += 2)
+            {
+                args[i] = "-i";
+                args[i + 1] = GetAbsolutePathFromRelative(programAbsolutePath, relativePath + entryRoutes[i / 2]); 
+            }
+            args[args.Length - 2] = "-o";
+            args[args.Length - 1] = GetAbsolutePathFromRelative(programAbsolutePath, relativePath + exitRoute);
+
+            for (int i = 0; i < args.Length; i += 2)
+            {
+                Console.WriteLine(args[i] + " " + args[i + 1]);
+            }
+
+            //Para poder cambiar el nombre de la imagen copiada
+
+            //string filePath = @"C:\Users\javie\Desktop\1 DAM\BD\Diagrama SUPERTIENDA (1).png"; // Reemplaza con la ruta de tu archivo
+
+            //ChangeNameImage.CopyImageFile(filePath);
+        }
+
+        public static string GetAbsolutePathFromRelative(string absolutePath, string relativePath)
+        {
+            try
+            {
+                // Combina la ruta absoluta con la ruta relativa
+                string combinedPath = Path.Combine(absolutePath, relativePath);
+
+                // Obtiene la ruta absoluta final normalizando la ruta combinada
+                string finalAbsolutePath = Path.GetFullPath(combinedPath);
+
+                //Console.WriteLine($"La ruta absoluta final es: {finalAbsolutePath}");
+                return finalAbsolutePath;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+
+}
