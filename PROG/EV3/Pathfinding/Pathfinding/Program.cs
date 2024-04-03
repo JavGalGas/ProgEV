886f5a2cf226cf7d906feb748640012c4fc289ee
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-04-03 21:07:13 +0200

    copia 18

--- PROG/EV3/Pathfinding/Pathfinding/Program.cs
+++ PROG/EV3/Pathfinding/Pathfinding/Program.cs
@@ -12,12 +12,12 @@ namespace Pathfinding
             //ruta relativa del directorio Pathfinding a partir de programAbsolutePath
             string relativePath = @"..\..\..\..\..\";
 
-            string[] routes = new string[0];
+                    //string[] routes = new string[0];
 
             List<string> entryRoutes = new List<string>
             {
-                @"entry1\",
-                @"entry2\"
+                "entry1",
+                "entry2"
             };
             string exitRoute = @"exit\";            
 
@@ -36,35 +36,13 @@ namespace Pathfinding
                 Console.WriteLine(args[i] + " " + args[i + 1]);
             }
 
+            RemoveDuplicates(ref args);
             //Para poder cambiar el nombre de la imagen copiada
 
             //string filePath = @"C:\Users\javie\Desktop\1 DAM\BD\Diagrama SUPERTIENDA (1).png"; // Reemplaza con la ruta de tu archivo
 
             //ChangeNameImage.CopyImageFile(filePath);
-
-            string directorio = args[1]; // Cambia esto a la ruta del directorio que quieres revisar
-
-            try
-            {
-                if (Directory.Exists(directorio))
-                {
-                    string[] archivos = Directory.GetFiles(directorio);
-
-                    Console.WriteLine("Archivos en el directorio {0}:", directorio);
-                    foreach (string archivo in archivos)
-                    {
-                        Console.WriteLine(Path.GetFileName(archivo));
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("El directorio {0} no existe.", directorio);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("El proceso falló: {0}", e.ToString());
-            }
+            
         }
 
         public static string GetAbsolutePathFromRelative(string absolutePath, string relativePath)
@@ -83,6 +61,49 @@ namespace Pathfinding
             catch (Exception e)
             {
                 throw new Exception(e.Message);
+            }
+        }
+
+        public static void RemoveDuplicates(ref string[] args)//modificar
+        {
+            for (int i = 1;i < args.Length -1;i+=2) 
+            {
+                string directorio = args[i]; // Cambia esto a la ruta del directorio que quieres revisar
+
+                try
+                {
+                    if (Directory.Exists(directorio))
+                    {
+                        string[] archivos = Directory.GetFiles(directorio);
+
+                        Console.WriteLine("Archivos en el directorio {0}:", directorio);
+                        foreach (string archivo in archivos)
+                        {
+                            Console.WriteLine(Path.GetFileName(archivo));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El directorio {0} no existe.", directorio);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("El proceso falló: {0}", e.ToString());
+                }
+            }
+        }
+
+        public static void CopyFileFromTo(string origin, string destiny)
+        {
+            try
+            {
+                File.Copy(origin, destiny, true);
+                Console.WriteLine("Archivo copiado exitosamente.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
