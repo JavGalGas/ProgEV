7571fcc3e25680546fa1e0c4a44d40b2bff187b6
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-04-05 19:44:22 +0200

    copia 19

--- PROG/EV3/Pathfinding/Pathfinding/Program.cs
+++ PROG/EV3/Pathfinding/Pathfinding/Program.cs
@@ -5,38 +5,23 @@ namespace Pathfinding
     internal class Program
     {
         static void Main(string[] args)
-        {//pasar a hacer nuevos arrays de string en args
+        {
+            List<string> entryRoutes = args.Take(args.Length-1).ToList();
+            string exitRoute = args[args.Length-1];            
 
-            //consigue la ruta absoluta del programa en ejecución (ddl)
-            string programAbsolutePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            //ruta relativa del directorio Pathfinding a partir de programAbsolutePath
-            string relativePath = @"..\..\..\..\..\";
-
-                    //string[] routes = new string[0];
-
-            List<string> entryRoutes = new List<string>
+            for (int i = 0; i< entryRoutes.Count; i++)
             {
-                "entry1",
-                "entry2"
-            };
-            string exitRoute = @"exit\";            
-
-            args = new string[entryRoutes.Count*2 + 2];
-
-            for (int i = 0; i < args.Length - 2; i += 2)
-            {
-                args[i] = "-i";
-                args[i + 1] = GetAbsolutePathFromRelative(programAbsolutePath, relativePath + entryRoutes[i / 2]); 
-            }
-            args[args.Length - 2] = "-o";
-            args[args.Length - 1] = GetAbsolutePathFromRelative(programAbsolutePath, relativePath + exitRoute);
-
-            for (int i = 0; i < args.Length; i += 2)
-            {
-                Console.WriteLine(args[i] + " " + args[i + 1]);
+                entryRoutes[i] = Path.GetFullPath(entryRoutes[i]);
             }
 
-            RemoveDuplicates(ref args);
+            foreach (string route in entryRoutes)
+            {
+                Console.WriteLine(route);
+            }
+
+            //RemoveDuplicates(ref args);
+
+
             //Para poder cambiar el nombre de la imagen copiada
 
             //string filePath = @"C:\Users\javie\Desktop\1 DAM\BD\Diagrama SUPERTIENDA (1).png"; // Reemplaza con la ruta de tu archivo
