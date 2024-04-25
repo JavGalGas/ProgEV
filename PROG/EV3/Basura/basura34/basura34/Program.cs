8c6d083938582f45dc22bf369b5e11fe6a3abb70
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-04-25 20:21:24 +0200

    copia 36

---
+++ PROG/EV3/Basura/basura34/basura34/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace basura34
+{
+    internal class Program
+    {
+        public delegate double MathFunction(double x);
+        public static double CalculateIntegralArea(MathFunction f, double x0, double x1, double dx = 0.0001)
+        {
+            if (f == null)
+            {
+                return double.MinValue;
+            }
+            double resul = 0.0;
+            double y = 0.0;
+            while (x0 <= x1) 
+            { 
+                y = f(x0);
+                resul += y * dx;
+                x0 += dx;
+            }
+            //resul = (x1 - x0) * f(x0);
+
+            //resul = ((x1 - x0)) * (f(x0) + f(x1)) / 2;
+
+            //resul = ((x1 - x0) / 6) * (f(x0) + 4 * f((x0 + x1) / 2) + f(x1));
+            return resul;
+        }
+        static void Main(string[] args)
+        {
+            double integral = CalculateIntegralArea(x =>
+            {
+                return 2 * (x * x * x) + x;
+            }, 0.0, 5.0);
+
+            Console.WriteLine(integral);
+        }
+    }
+}
