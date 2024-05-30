442f4250daae9a0a68a2f5ec39a750de6f2a71ee
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-05-30 19:38:57 +0200

    copia 51

--- PROG/EV3/Basura/CharacterClass/CharacterClass/Stadistics.cs
+++ PROG/EV3/Basura/CharacterClass/CharacterClass/Stadistics.cs
@@ -2,16 +2,16 @@
 {
     public class Stadistics
     {
-        private int _attack = 10;
-        private int _defense = 10;
         private int _health = 10;
         private int _magic = 10;
+        private int _attack = 10;
+        private int _defense = 10;
         private int _speed = 10;
 
-        public int Attack => _attack;
-        public int Defense => _defense;
         public int Health => _health;
         public int Magic => _magic;
+        public int Attack => _attack;
+        public int Defense => _defense;
         public int Speed => _speed;
 
         public Stadistics()
@@ -19,16 +19,49 @@
 
         }
 
-        public Stadistics(int attack, int defense, int health, int magic, int speed)
+        public Stadistics(int health, int magic, int attack, int defense, int speed)
         {
-            _attack = attack;
-            _defense = defense;
-            _health = health;
-            _magic = magic;
-            _speed = speed;
+            SetHealth(health);
+            SetMagic(magic);
+            SetAttack(attack);
+            SetDefense(defense);
+            SetSpeed(speed);
         }
 
+        public void SetAttack(int attack)
+        {
+            if (attack <= 0) 
+                return;
+            _attack = attack;
+        }
 
+        public void SetDefense(int defense)
+        {
+            if (defense <= 0)
+                return;
+            _defense = defense;
+        }
+
+        public void SetHealth(int health)
+        {
+            if (health <= 0)
+                return;
+            _health = health;
+        }
+
+        public void SetMagic(int magic)
+        {
+            if (magic <= 0)
+                return;
+            _magic = magic;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            if (speed <= 0)
+                return;
+            _speed = speed;
+        }
 
 
     }
--- PROG/EV3/Basura/CharacterClass/CharacterClass/Tank.cs
+++ PROG/EV3/Basura/CharacterClass/CharacterClass/Tank.cs
@@ -9,18 +9,16 @@ namespace CharacterClass
     public class Tank : Character
     {
         private long _id;
-        private Stadistics _stad = GetStadistics();
+        private Stadistics _stad;
         public Tank(long id, string name, int level, int position, int element, ClassCharacter @class, Stadistics stadistics) : base(id, name, level, position, element, @class, stadistics)
         { 
             _id = id;
+            _stad = GetStadistics();
         }
 
         private Stadistics GetStadistics()
         {
-            return new Stadistics()
-            {
-                
-            };
+            return new Stadistics(10,);
         }
     }
 }
